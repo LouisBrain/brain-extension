51efa328f25fd46a0212976f5b9b4e8d381f0201
LouisLallier <86591552+LouisLallier@users.noreply.github.com>
2023-05-16 16:20:17 +0200

    rdy to build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import DetailsPage from "./pages/DetailsPage";
+import "./mouseTracker.css";
+import { useState } from "react";
 
 function App() {
+  const [language, setLanguage] = useState("fr");
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className="App">
+      <div className="flex justify-end pr-3 h-10 w-full bg-transparent fixed top-0 left-0 z-10">
+        <div
+          className="h-10 "
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          {language === "fr" ? (
+            <img
+              onClick={() => setLanguage("fr")}
+              className="h-10 cursor-pointer"
+              src="https://storage.googleapis.com/cdnbrainsecurity/cybersensible/imgs/flags/France.png"
+              alt="FR_flag"
+            />
+          ) : (
+            <img
+              onClick={() => setLanguage("en")}
+              className="h-10 cursor-pointer"
+              src="https://storage.googleapis.com/cdnbrainsecurity/cybersensible/imgs/flags/United-Kingdoms.png"
+              alt="UK_flag"
+            />
+          )}
+          {isHover && (
+            <div className="">
+              {language === "fr" ? (
+                <img
+                  onClick={() => setLanguage("en")}
+                  className="h-10 cursor-pointer bg-transparent"
+                  src="https://storage.googleapis.com/cdnbrainsecurity/cybersensible/imgs/flags/United-Kingdoms.png"
+                  alt="UK_flag"
+                />
+              ) : (
+                <img
+                  onClick={() => setLanguage("fr")}
+                  className="h-10 cursor-pointer bg-transparent"
+                  src="https://storage.googleapis.com/cdnbrainsecurity/cybersensible/imgs/flags/France.png"
+                  alt="FR_flag"
+                />
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/detail/:id" element={<DetailsPage />} />
+          <Route path="/" element={<HomePage language={language} />} />
         </Routes>
       </Router>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,262 +1,244 @@
-import gif01Fr from "../assets/brain_gif_reflex/fr/01.gif";
-import gif02Fr from "../assets/brain_gif_reflex/fr/02.gif";
-import gif03Fr from "../assets/brain_gif_reflex/fr/03.gif";
-import gif04Fr from "../assets/brain_gif_reflex/fr/04.gif";
-import gif05Fr from "../assets/brain_gif_reflex/fr/05.gif";
-import gif06Fr from "../assets/brain_gif_reflex/fr/06.gif";
-import gif07Fr from "../assets/brain_gif_reflex/fr/07.gif";
-import gif08Fr from "../assets/brain_gif_reflex/fr/08.gif";
-import gif09Fr from "../assets/brain_gif_reflex/fr/09.gif";
-import gif10Fr from "../assets/brain_gif_reflex/fr/10.gif";
-import gif11Fr from "../assets/brain_gif_reflex/fr/11.gif";
-import gif12Fr from "../assets/brain_gif_reflex/fr/12.gif";
-import gif13Fr from "../assets/brain_gif_reflex/fr/13.gif";
-import gif14Fr from "../assets/brain_gif_reflex/fr/14.gif";
-import gif15Fr from "../assets/brain_gif_reflex/fr/15.gif";
-import gif16Fr from "../assets/brain_gif_reflex/fr/16.gif";
-import gif17Fr from "../assets/brain_gif_reflex/fr/17.gif";
-import gif18Fr from "../assets/brain_gif_reflex/fr/18.gif";
+import "./HomePage.css";
 
-import gif01En from "../assets/brain_gif_reflex/en/01e.gif";
-import gif02En from "../assets/brain_gif_reflex/en/02e.gif";
-import gif03En from "../assets/brain_gif_reflex/en/03e.gif";
-import gif04En from "../assets/brain_gif_reflex/en/04e.gif";
-import gif05En from "../assets/brain_gif_reflex/en/05e.gif";
-import gif06En from "../assets/brain_gif_reflex/en/06e.gif";
-import gif07En from "../assets/brain_gif_reflex/en/07e.gif";
-import gif08En from "../assets/brain_gif_reflex/en/08e.gif";
-import gif09En from "../assets/brain_gif_reflex/en/09e.gif";
-import gif10En from "../assets/brain_gif_reflex/en/10e.gif";
-import gif11En from "../assets/brain_gif_reflex/en/11e.gif";
-import gif12En from "../assets/brain_gif_reflex/en/12e.gif";
-import gif13En from "../assets/brain_gif_reflex/en/13e.gif";
-import gif14En from "../assets/brain_gif_reflex/en/14e.gif";
-import gif15En from "../assets/brain_gif_reflex/en/15e.gif";
-import gif16En from "../assets/brain_gif_reflex/en/16e.gif";
-import gif17En from "../assets/brain_gif_reflex/en/17e.gif";
-import gif18En from "../assets/brain_gif_reflex/en/18e.gif";
-
-import brain from "../assets/brainGif.gif";
-import React, { useState } from "react";
-
-const HomePage = () => {
-  const [language, setLanguage] = useState("fr");
-
+const HomePage = ({ language }) => {
   const tabGif = [
     {
       fr: {
-        mainGif: gif01Fr,
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/01fr.gif",
         url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif01En,
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/01en.gif",
         url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
-
     {
       fr: {
-        mainGif: gif02Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09633ag",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/02fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif02En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09633ag",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/02en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif03Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091821u",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/03fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif03En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091821u",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/03en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif04Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091128ab",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/04fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif04En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091128ab",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/04en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif05Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09534ah",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/05fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif05En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09534ah",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/05en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif06Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091722v",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/06fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif06En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091722v",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/06en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif07Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091029ac",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/07fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif07En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091029ac",
-      },
-    },
-
-    {
-      fr: {
-        mainGif: gif08Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091524x",
-      },
-      en: {
-        mainGif: gif08En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091524x",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/07en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif09Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091623w",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/08fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif09En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091623w",
-      },
-    },
-
-    {
-      fr: {
-        mainGif: gif10Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09336aj",
-      },
-      en: {
-        mainGif: gif10En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09336aj",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/08en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif11Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091524x",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/09fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif11En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091524x",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/09en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif12Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09930ad",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/10fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif12En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09930ad",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/10en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif13Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09237ak",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/11fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif13En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09237ak",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/11en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif14Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091425y",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/12fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif14En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091425y",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/12en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif15Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09831ae",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/13fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif15En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09831ae",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/13en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif16Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/rt",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/14fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif16En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/rt",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/14en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif17Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091326z",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/15fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif17En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091326z",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/15en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
     {
       fr: {
-        mainGif: gif18Fr,
-        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e09732af",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/16fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
       en: {
-        mainGif: gif18En,
-        url: "https://en.play.brainsecurity.io/Tree/lesson006/e09732af",
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/16en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
+      },
+    },
+    {
+      fr: {
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/17fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
+      },
+      en: {
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/17en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
+      },
+    },
+    {
+      fr: {
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/fr/18fr.gif",
+        url: "https://fr.play.brainsecurity.io/Tree/lesson006/e091227aa",
+      },
+      en: {
+        mainGif:
+          "https://storage.googleapis.com/cdnbrainsecurity/reflex_GIF/en/18en.gif",
+        url: "https://en.play.brainsecurity.io/Tree/lesson006/e091227aa",
       },
     },
   ];
 
-  const switchLanguage = () => {
-    setLanguage(language === "fr" ? "en" : "fr"); // switch entre 'fr' et 'en'
-  };
-
   return (
     <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden">
       <div className="absolute z-0 w-full h-full">
-        <img className="w-full" src={brain} alt="brainGif" />
+        <img
+          className="w-full"
+          src="https://storage.googleapis.com/cdnbrainsecurity/cybersensible/medias/brainbgdark.gif"
+          alt="brainGif"
+        />
       </div>
 
       <div className="absolute top-0 flex justify-center items-center h-full w-full z-10">
-        <button
-          onClick={switchLanguage}
-          className="absolute top-0 right-0 m-4 p-2 bg-black text-white"
-        >
-          Switch Language
-        </button>
         <div className="text-green-600 grid grid-cols-6 gap-4 z-10">
           {tabGif.map((item, i) => {
             return (
-              <a
-                href={item[language].url}
-                target="_blank"
-                rel="noreferrer"
-                className="h-[170px] w-[140px]"
-                key={i}
-              >
-                <img className="" src={item[language].mainGif} alt="GIF" />
-              </a>
+              <div className="card rainbow-border w-[130px] " key={i}>
+                <a href={item[language].url} target="_blank" rel="noreferrer">
+                  <img className="" src={item[language].mainGif} alt="GIF" />
+                </a>
+              </div>
             );
           })}
         </div>
